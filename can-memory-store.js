8f0573bda37722f194fe3da129a85f372ab11df4
Justin Meyer <justinbmeyer@gmail.com>
2018-04-27 16:44:31 -0500

    rough docs

--- can-memory-store.js
+++ can-memory-store.js
@@ -1,47 +1,3 @@
-/**
- * @module can-memory-storea can-memory-store
- * @parent can-data-modeling
- * @collection can-infrastructure
- * @group can-connect/data/memory-cache/memory-cache.data-methods data methods
- *
- * Saves raw data in JavaScript memory that disappears when the page refreshes.
- *
- * @signature `memoryCache( baseConnection )`
- *
- *   Creates a cache of instances and a cache of sets of instances that is
- *   accessible to read via [can-connect/data/memory-cache/memory-cache.getSets],
- *   [can-connect/data/memory-cache/memory-cache.getData], and [can-connect/data/memory-cache/memory-cache.getListData].
- *   The caches are updated via [can-connect/data/memory-cache/memory-cache.createData],
- *   [can-connect/data/memory-cache/memory-cache.updateData], [can-connect/data/memory-cache/memory-cache.destroyData],
- *   and [can-connect/data/memory-cache/memory-cache.updateListData].
- *
- *   [can-connect/data/memory-cache/memory-cache.createData],
- *   [can-connect/data/memory-cache/memory-cache.updateData],
- *   [can-connect/data/memory-cache/memory-cache.destroyData] are able to move items in and out
- *   of sets.
- *
- * @body
- *
- * ## Use
- *
- * `data/memory-cache` is often used with a caching strategy like [can-connect/fall-through-cache/fall-through-cache] or
- * [can-connect/cache-requests/cache-requests].
- *
- * ```js
- * var cacheConnection = connect([
- *   require("can-connect/data/memory-cache/memory-cache")
- * ],{});
- *
- * var todoConnection = connect([
- *   require("can-connect/data/url/url"),
- *   require("can-connect/fall-through-cache/fall-through-cache")
- * ],
- * {
- *   url: "/services/todos",
- *   cacheConnection: cacheConnection
- * });
- * ```
- */
 var canReflect = require("can-reflect");
 var sortObject = require("can-sort-object");
 var updateExceptId = require("can-diff/update-deep-except-identity/update-deep-except-identity");
@@ -66,9 +22,9 @@ module.exports = function memoryStore(baseConnection){
 			return this._sets;
 		},
 
-		__getListData: function(set){
+		__getListData: function(query){
 			var setsData = this.getSetData();
-			var setData = setsData[JSON.stringify(sortObject(set))];
+			var setData = setsData[JSON.stringify(sortObject(query))];
 			if(setData) {
 				return setData.items;
 			}
@@ -77,41 +33,41 @@ module.exports = function memoryStore(baseConnection){
 		getInstance: function(id){
 			return this._instances[id];
 		},
-		getInstanceFromProps: function(props) {
-			var id = canReflect.getIdentity(props, this.queryLogic.schema);
+		getInstanceFromProps: function(record) {
+			var id = canReflect.getIdentity(record, this.queryLogic.schema);
 			return this.getInstance(id);
 		},
 		removeSet: function(setKey, noUpdate) {
-			var sets = this.getSetData();
-			delete sets[setKey];
+			var queries = this.getSetData();
+			delete queries[setKey];
 			if(noUpdate !== true) {
 				this.updateSets();
 			}
 		},
 		updateSets: function(){ },
 
-		updateInstance: function(props) {
-			var instance = this.getInstanceFromProps(props);
+		updateInstance: function(record) {
+			var instance = this.getInstanceFromProps(record);
 			if(instance) {
-				updateExceptId(instance, props, this.queryLogic.schema);
+				updateExceptId(instance, record, this.queryLogic.schema);
 			} else {
-				var id = canReflect.getIdentity(props, this.queryLogic.schema);
-				instance = this._instances[id] = props;
+				var id = canReflect.getIdentity(record, this.queryLogic.schema);
+				instance = this._instances[id] = record;
 			}
 			return instance;
 		},
-		// Updates a set
+		// Updates a query
 		updateSet: function(setDatum, items, newSet) {
 			var newSetKey = newSet ? JSON.stringify(sortObject(newSet)) : setDatum.setKey;
 			if(newSet) {
 				// if the setKey is changing
 				if(newSetKey !== setDatum.setKey) {
 					// add the new one
-					var sets = this.getSetData();
+					var queries = this.getSetData();
 					var oldSetKey = setDatum.setKey;
-					sets[newSetKey] = setDatum;
+					queries[newSetKey] = setDatum;
 					setDatum.setKey = newSetKey;
-					setDatum.set = canReflect.assignMap({},newSet);
+					setDatum.query = canReflect.assignMap({},newSet);
 					// remove the old one
 					this.removeSet(oldSetKey);
 				}
@@ -126,15 +82,15 @@ module.exports = function memoryStore(baseConnection){
 				self.updateInstance(item);
 			});
 		},
-		addSet: function(set, data) {
+		addSet: function(query, data) {
 			var items = getItems(data);
-			var sets = this.getSetData();
-			var setKey = JSON.stringify(sortObject(set));
+			var queries = this.getSetData();
+			var setKey = JSON.stringify(sortObject(query));
 
-			sets[setKey] = {
+			queries[setKey] = {
 				setKey: setKey,
 				items: items,
-				set: canReflect.assignMap({},set)
+				query: canReflect.assignMap({},query)
 			};
 
 			var self = this;
@@ -145,7 +101,7 @@ module.exports = function memoryStore(baseConnection){
 			this.updateSets();
 		},
 		_eachSet: function(cb){
-			var sets = this.getSetData();
+			var queries = this.getSetData();
 			var self = this;
 			var loop = function(setDatum, setKey) {
 				return cb.call(self, setDatum, setKey, function(){
@@ -154,8 +110,8 @@ module.exports = function memoryStore(baseConnection){
 				});
 			};
 
-			for(var setKey in sets) {
-				var setDatum = sets[setKey];
+			for(var setKey in queries) {
+				var setDatum = queries[setKey];
 				var result = loop(setDatum, setKey);
 				if(result !== undefined) {
 					return result;
@@ -163,55 +119,55 @@ module.exports = function memoryStore(baseConnection){
 			}
 		},
 		_getSets: function(){
-			var sets = [],
+			var queries = [],
 				setsData = this.getSetData();
 			for(var prop in setsData) {
-				sets.push(setsData[prop].set);
+				queries.push(setsData[prop].query);
 			}
-			return sets;
+			return queries;
 		},
 		// ## External interface
 
 		/**
-		 * @function can-connect/data/memory-cache/memory-cache.getSets getSets
-		 * @parent can-connect/data/memory-cache/memory-cache.data-methods
+		 * @function can-memory-store.getQueries getQueries
+		 * @parent can-memory-store.data-methods
 		 *
-		 * Returns the sets contained within the cache.
+		 * Returns the queries contained within the cache.
 		 *
-		 * @signature `connection.getSets()`
+		 * @signature `connection.getQueries()`
 		 *
-		 *   Returns the sets added by [can-connect/data/memory-cache/memory-cache.updateListData].
+		 *   Returns the queries added by [can-memory-store.updateListData].
 		 *
-		 *   @return {Promise<Array<can-set/Set>>} A promise that resolves to the list of sets.
+		 *   @return {Promise<Array<can-query-logic/query>>} A promise that resolves to the list of queries.
 		 *
 		 * @body
 		 *
 		 * ## Use
 		 *
-		 * ```
+		 * ```js
 		 * connection.getSets() //-> Promise( [{type: "completed"},{user: 5}] )
 		 * ```
 		 *
 		 */
 		getSets: function(){
 
+			return this.getQueries();
+		},
+		getQueries: function(){
 			return Promise.resolve(this._getSets());
 		},
-
 		/**
-		 * @function can-connect/data/memory-cache/memory-cache.clear clear
-		 * @parent can-connect/data/memory-cache/memory-cache.data-methods
+		 * @function can-memory-store.clear clear
+		 * @parent can-memory-store.data-methods
 		 *
-		 * Resets the memory cache so it contains nothing.
+		 * Resets the memory store so it contains nothing.
 		 *
 		 * @signature `connection.clear()`
 		 *
 		 *   Removes all instances and lists being stored in memory.
 		 *
-		 *   ```
-		 *   var cacheConnection = connect([
-		 *     require("can-connect/data/memory-cache/memory-cache")
-		 *   ],{});
+		 *   ```js
+		 *   memoryStore({queryLogic: new QueryLogic()});
 		 *
 		 *   cacheConnection.updateInstance({id: 5, name: "justin"});
 		 *
@@ -230,32 +186,32 @@ module.exports = function memoryStore(baseConnection){
 			this._sets = {};
 		},
 		/**
-		 * @function can-connect/data/memory-cache/memory-cache.getListData getListData
-		 * @parent can-connect/data/memory-cache/memory-cache.data-methods
+		 * @function can-memory-store.getListData getListData
+		 * @parent can-memory-store.data-methods
 		 *
-		 * Gets a set of data from the memory cache.
+		 * Gets a list of data from the memory store.
 		 *
-		 * @signature `connection.getListData(set)`
+		 * @signature `connection.getListData(query)`
 		 *
-		 *   Goes through each set add by [can-connect/data/memory-cache/memory-cache.updateListData]. If
-		 *   `set` is a subset, uses [can-connect/base/base.queryLogic] to get the data for the requested `set`.
+		 *   Goes through each query add by [can-memory-store.updateListData]. If
+		 *   `query` is a subset, uses [can-connect/base/base.queryLogic] to get the data for the requested `query`.
 		 *
-		 *   @param {can-set/Set} set An object that represents the data to load.
+		 *   @param {can-query-logic/query} query An object that represents the data to load.
 		 *
-		 *   @return {Promise<can-connect.listData>} A promise that resolves if `set` is a subset of
-		 *   some data added by [can-connect/data/memory-cache/memory-cache.updateListData].  If it is not,
+		 *   @return {Promise<can-connect.listData>} A promise that resolves if `query` is a subset of
+		 *   some data added by [can-memory-store.updateListData].  If it is not,
 		 *   the promise is rejected.
 		 */
-		getListData: function(set){
-			set = set || {};
-			var listData = this.getListDataSync(set);
+		getListData: function(query){
+			query = query || {};
+			var listData = this.getListDataSync(query);
 			if(listData) {
 				return Promise.resolve(listData);
 			}
 			return Promise.reject({
 				title: "no data",
 				status: "404",
-				detail: "No data available for this set.\nAvailable sets: "+
+				detail: "No data available for this query.\nAvailable queries: "+
 					JSON.stringify(this._getSets())
 			});
 
@@ -264,70 +220,70 @@ module.exports = function memoryStore(baseConnection){
 		 * @function can-connect/data/memory-cache.getListDataSync getListDataSync
 		 * @parent can-connect/data/memory-cache.data-methods
 		 *
-		 * Synchronously gets a set of data from the memory cache.
+		 * Synchronously gets a query of data from the memory cache.
 		 *
-		 * @signature `connection.getListDataSync(set)`
+		 * @signature `connection.getListDataSync(query)`
 		 * @hide
 		 */
-		getListDataSync: function(set){
-			var sets = this._getSets();
-			for(var i = 0; i < sets.length; i++) {
-				var checkSet = sets[i];
+		getListDataSync: function(query){
+			var queries = this._getSets();
+			for(var i = 0; i < queries.length; i++) {
+				var checkSet = queries[i];
 
-				if( this.queryLogic.isSubset(set, checkSet) ) {
+				if( this.queryLogic.isSubset(query, checkSet) ) {
 					var source = this.__getListData(checkSet);
-					return this.queryLogic.filterMembersAndGetCount(set, checkSet, source);
+					return this.queryLogic.filterMembersAndGetCount(query, checkSet, source);
 				}
 			}
 		},
 		// a sync method used by can-fixture.
-		_getListData: function(set){
-			return this.getListDataSync(set);
+		_getListData: function(query){
+			return this.getListDataSync(query);
 		},
 		/**
-		 * @function can-connect/data/memory-cache/memory-cache.updateListData updateListData
-		 * @parent can-connect/data/memory-cache/memory-cache.data-methods
+		 * @function can-memory-store.updateListData updateListData
+		 * @parent can-memory-store.data-methods
 		 *
-		 * Saves a set of data in the cache.
+		 * Saves a query of data in the cache.
 		 *
-		 * @signature `connection.updateListData(listData, set)`
+		 * @signature `connection.updateListData(listData, query)`
 		 *
-		 *   Tries to merge this set of data with any other saved sets of data. If
-		 *   unable to merge this data, saves the set by itself.
+		 *   Tries to merge this query of data with any other saved queries of data. If
+		 *   unable to merge this data, saves the query by itself.
 		 *
-		 *   @param {can-connect.listData} listData The data that belongs to `set`.
-		 *   @param {can-set/Set} set The set `listData` belongs to.
+		 *   @param {can-connect.listData} listData The data that belongs to `query`.
+		 *   @param {can-query-logic/query} query The query `listData` belongs to.
 		 *   @return {Promise} Promise resolves if and when the data has been successfully saved.
 		 */
-		updateListData: function(data, set){
-			set = set || {};
+		updateListData: function(data, query){
+			query = query || {};
 
 			var clonedData = canReflect.serialize(data);
 			var items = getItems(clonedData);
-			var sets = this.getSetData();
+			var queries = this.getSetData();
 			var self = this;
 
-			for(var setKey in sets) {
-				var setDatum = sets[setKey];
-				var union = this.queryLogic.union(setDatum.set, set);
+			for(var setKey in queries) {
+				var setDatum = queries[setKey];
+				var union = this.queryLogic.union(setDatum.query, query);
 				if( this.queryLogic.isDefinedAndHasMembers(union) ) {
-					// copies so we don't pass the same set object
-					var getSet = canReflect.assignMap({},setDatum.set);
+					// copies so we don't pass the same query object
+					var getSet = canReflect.assignMap({},setDatum.query);
 					return this.getListData(getSet).then(function(setData){
 
-						self.updateSet(setDatum, self.queryLogic.unionMembers(getSet, set, getItems(setData), items ), union);
+						self.updateSet(setDatum, self.queryLogic.unionMembers(getSet, query, getItems(setData), items ), union);
 					});
 				}
 			}
 
-			this.addSet(set, clonedData);
-			// setData.push({set: set, items: data});
+			this.addSet(query, clonedData);
+			// setData.push({query: query, items: data});
 			return Promise.resolve();
 		},
 
 		/**
-		 * @function can-connect/data/memory-cache/memory-cache.getData getData
-		 * @parent can-connect/data/memory-cache/memory-cache.data-methods
+		 * @function can-memory-store.getData getData
+		 * @parent can-memory-store.data-methods
 		 *
 		 * Get an instance's data from the memory cache.
 		 *
@@ -358,23 +314,23 @@ module.exports = function memoryStore(baseConnection){
 
 
 		/**
-		 * @function can-connect/data/memory-cache/memory-cache.createData createData
-		 * @parent can-connect/data/memory-cache/memory-cache.data-methods
+		 * @function can-memory-store.createData createData
+		 * @parent can-memory-store.data-methods
 		 *
 		 * Called when an instance is created and should be added to cache.
 		 *
-		 * @signature `connection.createData(props)`
+		 * @signature `connection.createData(record)`
 		 *
-		 *   Adds `props` to the stored list of instances. Then, goes
-		 *   through every set and adds props the sets it belongs to.
+		 *   Adds `record` to the stored list of instances. Then, goes
+		 *   through every query and adds record the queries it belongs to.
 		 */
-		createData: function(props){
+		createData: function(record){
 			var self = this;
-			var instance = this.updateInstance(props);
+			var instance = this.updateInstance(record);
 
 			this._eachSet(function(setDatum, setKey, getItems){
-				if(this.queryLogic.isMember(setDatum.set, instance )) {
-					self.updateSet(setDatum, self.queryLogic.insert( setDatum.set,  getItems(), instance), setDatum.set);
+				if(this.queryLogic.isMember(setDatum.query, instance )) {
+					self.updateSet(setDatum, self.queryLogic.insert( setDatum.query,  getItems(), instance), setDatum.query);
 				}
 			});
 
@@ -382,42 +338,42 @@ module.exports = function memoryStore(baseConnection){
 		},
 
 		/**
-		 * @function can-connect/data/memory-cache/memory-cache.updateData updateData
-		 * @parent can-connect/data/memory-cache/memory-cache.data-methods
+		 * @function can-memory-store.updateData updateData
+		 * @parent can-memory-store.data-methods
 		 *
 		 * Called when an instance is updated.
 		 *
-		 * @signature `connection.updateData(props)`
+		 * @signature `connection.updateData(record)`
 		 *
-		 *   Overwrites the stored instance with the new props. Then, goes
-		 *   through every set and adds or removes the instance if it belongs or not.
+		 *   Overwrites the stored instance with the new record. Then, goes
+		 *   through every query and adds or removes the instance if it belongs or not.
 		 */
-		updateData: function(props){
+		updateData: function(record){
 			var self = this;
 
-			if(this.errorOnMissingRecord && !this.getInstanceFromProps(props)) {
-				var id = canReflect.getIdentity(props, this.queryLogic.schema);
+			if(this.errorOnMissingRecord && !this.getInstanceFromProps(record)) {
+				var id = canReflect.getIdentity(record, this.queryLogic.schema);
 				return Promise.reject({
 					title: "no data",
 					status: "404",
 					detail: "No record with matching identity ("+id+")."
 				});
 			}
-			var instance = this.updateInstance(props);
+			var instance = this.updateInstance(record);
 
-			// for now go through every set, if this belongs, add it or update it, otherwise remove it
+			// for now go through every query, if this belongs, add it or update it, otherwise remove it
 			this._eachSet(function(setDatum, setKey, getItems){
-				// if props belongs
+				// if record belongs
 				var items = getItems();
 				var index = indexOf(items, instance, self.queryLogic.schema );
 
-				if( this.queryLogic.isSubset(instance, setDatum.set) ) {
+				if( this.queryLogic.isSubset(instance, setDatum.query) ) {
 
 					// if it's not in, add it
 					if(index === -1) {
 						// how to insert things together?
 
-						self.updateSet(setDatum, self.queryLogic.insert( setDatum.set,  getItems(), instance ) );
+						self.updateSet(setDatum, self.queryLogic.insert( setDatum.query,  getItems(), instance ) );
 					} else {
 						// otherwise add it
 						items.splice(index,1, instance);
@@ -436,19 +392,19 @@ module.exports = function memoryStore(baseConnection){
 		},
 
 		/**
-		 * @function can-connect/data/memory-cache/memory-cache.destroyData destroyData
-		 * @parent can-connect/data/memory-cache/memory-cache.data-methods
+		 * @function can-memory-store.destroyData destroyData
+		 * @parent can-memory-store.data-methods
 		 *
 		 * Called when an instance should be removed from the cache.
 		 *
-		 * @signature `connection.destroyData(props)`
+		 * @signature `connection.destroyData(record)`
 		 *
-		 *   Goes through each set of data and removes any data that matches
-		 *   `props`'s [can-connect/base/base.id]. Finally removes this from the instance store.
+		 *   Goes through each query of data and removes any data that matches
+		 *   `record`'s [can-connect/base/base.id]. Finally removes this from the instance store.
 		 */
-		destroyData: function(props){
-			var id = canReflect.getIdentity(props,  this.queryLogic.schema);
-			if(this.errorOnMissingRecord && !this.getInstanceFromProps(props)) {
+		destroyData: function(record){
+			var id = canReflect.getIdentity(record,  this.queryLogic.schema);
+			if(this.errorOnMissingRecord && !this.getInstanceFromProps(record)) {
 
 				return Promise.reject({
 					title: "no data",
@@ -460,9 +416,9 @@ module.exports = function memoryStore(baseConnection){
 			var self = this;
 
 			this._eachSet(function(setDatum, setKey, getItems){
-				// if props belongs
+				// if record belongs
 				var items = getItems();
-				var index = indexOf( items, props, self.queryLogic.schema );
+				var index = indexOf( items, record, self.queryLogic.schema );
 
 				if(index !== -1){
 					// otherwise remove it
@@ -471,7 +427,7 @@ module.exports = function memoryStore(baseConnection){
 				}
 			});
 			delete this._instances[id];
-			return Promise.resolve(canReflect.assignMap({},props));
+			return Promise.resolve(canReflect.assignMap({},record));
 		}
 	});
 
